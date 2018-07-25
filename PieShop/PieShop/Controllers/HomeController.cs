d6b2d9a40af2eb64e5ae7a93e536cb944b43c7a2
David Stewart <david.stewart@communities-ni.gov.uk>
2018-07-25 15:31:24 +0100

    HomeController and Index action added

---
+++ PieShop/PieShop/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PieShop.Interfaces;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository) {
+            _pieRepository = pieRepository;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(_pieRepository.GetPies());
+        }
+    }
+}
